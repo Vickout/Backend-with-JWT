8599239cc8404d2780573c221eebcbd45bd820f1
Victor Lourenço <vlourenco.m@gmail.com>
2021-01-12 20:01:07 -0300

    Created controllers and middleware

---
+++ src/app/controllers/UserController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+
+class UserController {
+  static index(req: Request, res: Response): Response {
+    return res.send({ userId: req.userId });
+  }
+
+  static async store(req: Request, res: Response): Promise<Response> {
+    const repository = getRepository(User);
+    const { email, password } = req.body;
+
+    const userExists = await repository.findOne({ where: email });
+
+    if (userExists) {
+      return res.sendStatus(409);
+    }
+
+    const user = repository.create({ email, password });
+    await repository.save(user);
+
+    return res.json(user);
+  }
+}
+
+export default UserController;
