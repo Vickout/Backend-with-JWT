01913146ea038452d7d3373514b37dae4898180d
Victor Lourenço <vlourenco.m@gmail.com>
2021-01-15 00:50:19 -0300

    Inserting cors on index file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 
 import './database/connect';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
